68efce52ed26962b6d453bd680e3bc45dafa49dd
njpowers7915 <njpowers7915@gmail.com>
2018-10-01 12:04:36 -0400

    Make SongSearchForm

--- client/src/components/SearchSongForm.js
+++ client/src/components/SearchSongForm.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const CreatePlaylistForm = ({onSubmit}) =>
+const SearchSongForm = ({onSubmit}) =>
   let input
   return (
     <div>
       <form onSubmit={e => { onSubmit(input.value) }}>
         <input ref={node => {input = node}} />
-        <button type="submit">Create Playlist</button>
+        <button type="submit">Search</button>
       </form>
     </div>
   )
 
-export default CreatePlaylistForm
+export default SearchSongForm
