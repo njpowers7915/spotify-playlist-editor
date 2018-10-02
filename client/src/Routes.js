6a747d2f46520353e54e7f1dbe0d2f104f0f79a4
njpowers7915 <njpowers7915@gmail.com>
2018-10-01 20:08:33 -0400

    Adding react-redux connection to container components

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import {Switch, Route} from 'react-router'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 ReactDOM.render((
   <Router>
     <React.Fragment>
-      <Route path="/" render={Home} />
-      <Route exact path="/signup" render={Signup} />
-      <Route exact path="/login" render={Login} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/login" component={Login} />
     </React.Fragment>
   </Router>),
   document.getElementById('root')
--- client/src/containers/PlaylistsContainer.js
+++ client/src/containers/PlaylistsContainer.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
 class PlaylistsContainer extends Component {
   constructor(props) {
