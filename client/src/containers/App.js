c09894339093394efd14e69d760cc2603dfa36aa
njpowers7915 <njpowers7915@gmail.com>
2018-10-02 11:11:31 -0400

    adding router to App.js

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //import './App.css';
-import PlaylistsContainer from './components/PlaylistsContainer'
+import WelcomeContainer from './WelcomeContainer'
+import HomeContainer from './HomeContainer'
+import PlaylistsShow from './PlaylistsShow'
 
-const playlists = [
-  {
-    title: "Chill Playlist"
-  },
-  {
-    title: "Gym Playlist"
-  }
-]
+const playlists = [{title: "Chill Playlist"}, {title: "Gym Playlist"}]
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1>Welcome to Spotify Playlist Editor</h1>
-        </div>
-        <PlaylistsContainer playlists={playlists} />
+        <Router>
+          <React.Fragment>
+            <Route exact path="/" render={WelcomeContainer} />
+            <Route exact path="/playlists" render={HomeContainer} />
+            <Route exact path="/playlists/:playlistid" render={PlaylistsShow} />
+          </React.Fragment>
+        </Router>
       </div>
-    );
+    )
   }
 }
+//class App extends Component {
+//  render() {
+//    return (
+//      <div className="App">
+//        <div className="App-header">
+//          <h1>Welcome to Spotify Playlist Editor</h1>
+//        </div>
+//        <PlaylistsContainer playlists={playlists} />
+//      </div>
+//    );
+//  }
+//}
 
 export default App;
