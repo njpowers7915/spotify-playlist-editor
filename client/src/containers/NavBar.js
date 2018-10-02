500641d5d0153ad259648969d9647883e40251bf
njpowers7915 <njpowers7915@gmail.com>
2018-10-02 11:19:18 -0400

    Adding links to NavBar

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-const NavBar = () =>
-  return (
-    <div>
-      NavBar
-    </div>
-  )
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () =>
+  <div>
+    <h3>Welcome user!</h3>
+    <NavLink to="/" exact style={link}
+      activeStyle={{background: 'darkblue'}}>Home</NavLink>
+    <NavLink to="/about" exact style={link}
+      activeStyle={{background: 'darkblue'}}>Logout</NavLink>
+  </div>;
+
+
+//const NavBar = () =>
+//  return (
+//    <div>
+//      NavBar
+//    </div>
+//  )
 
 export default NavBar
